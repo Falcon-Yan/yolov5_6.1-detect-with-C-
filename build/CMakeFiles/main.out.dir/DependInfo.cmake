38d249ad7e990a2336f82dd3062e91a7c6ebb3b4
yanchengzhuang521 <78789632+yanchengzhuang521@users.noreply.github.com>
2022-04-15 20:58:27 +0800

    Add files via upload

---
+++ build/CMakeFiles/main.out.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yan/workspace/OpenCVprojects/YOLOv5_6.1/main.cpp" "CMakeFiles/main.out.dir/main.cpp.o" "gcc" "CMakeFiles/main.out.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yan/workspace/OpenCVprojects/YOLOv5_6.1/build/CMakeFiles/yolov5.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.out.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/main.out"
+  "../bin/main.out.pdb"
+  "CMakeFiles/main.out.dir/main.cpp.o"
+  "CMakeFiles/main.out.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
